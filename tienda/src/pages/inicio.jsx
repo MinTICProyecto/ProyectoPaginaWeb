cb782f270b14d4d22b566fb355922c3457b878a6
Yojan Trujillo Trujillo <yojan2378@gmail.com>
2021-10-27 03:39:48 -0500

    Creacion pagina de inicio

---
+++ tienda/src/pages/inicio.jsx
@@ -0,0 +1,39 @@
+
+import { Button, Divider, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from '@mui/material/Typography';
+import Stack from "@mui/material/Stack";
+import GoogleIcon from '@mui/icons-material/Google';
+import "../styles/inicio.css";
+
+const Inicio = () => {
+    return (
+        <Box sx={{ backgroundColor: "#B6F4FF", display:"flex", justifyContent:"center", alignItems:"center", height:'100vh'}} >
+            <Box sx={{alignItems:"center"}}> 
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography gutterBottom variant="h4" component="div" sx={{ paddingLeft: 2 }}>
+                            Inicio de Sesion
+                        </Typography>
+                        <Divider />
+                        <Stack component="form" sx={{ '& .MuiTextField-root': { m: 3, width: '100ch' }, alignItems: "center", paddingTop: 5 }}>
+                            <TextField label="Escriba su correo" variant="outlined" />
+                            <TextField label="Escriba contrasena" type="password" variant="outlined" />
+                            <Button variant="contained" size="50" color="success" sx={{fontSize:17}}> Iniciar Sesion</Button>
+                            <Typography gutterBottom variant="h6" component="div" sx={{paddingTop:2, paddingBottom:2}}>
+                                O
+                            </Typography>
+                            <Button variant="contained" endIcon={<GoogleIcon />} color="warning" size="50" sx={{fontSize:17}}> Google</Button>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Box>
+        </Box>
+    );
+
+
+}
+
+export default Inicio;
